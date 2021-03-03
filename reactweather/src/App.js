44e27fa520fca917bafedfea671c1894f3cda303
e0marina <emilymarinaro@gmail.com>
2021-03-02 17:31:02 -0800

    initial files

---
+++ reactweather/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import CitySelector from './components/CitySelector'
+import './App.css';
+import { Container } from 'react-bootstrap';
+
+
+function App() {
+
+
+  return (
+    <Container className="App">
+      <CitySelector />
+    </Container>
+  );
+}
+
+export default App;
